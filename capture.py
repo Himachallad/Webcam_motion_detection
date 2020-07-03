37b6f306c96b89d361576be92333256b9f0cc5c8
Himachallad <nikhilpathania.nits@gmail.com>
2020-07-03 15:09:21 +0530

    Saving motion detection to CSV file

--- capture.py
+++ capture.py
@@ -1,18 +1,22 @@
-import cv2, time
+import cv2, pandas
+from datetime import datetime
 
 first_frame = None
 cap = cv2.VideoCapture(0)
+status_list = [None, None]
+times = []
+df = pandas.DataFrame(columns=["Start", "End"])
+
 while True: 
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+    status = 0
     if first_frame is None:
         first_frame = gray
         continue
 
     delta_frame = cv2.absdiff(first_frame, gray)
-    print(delta_frame)
 
     threshold_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     threshold_frame = cv2.dilate(threshold_frame, None, iterations=2)
@@ -20,17 +24,27 @@ while True:
     (cnts, _) = cv2.findContours(threshold_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    status_list.append(status)
 
+    if status_list[-2] != None and abs(status_list[-1] - status_list[-2]) == 1:
+        times.append(datetime.now())
     cv2.imshow("Camera", gray)
     cv2.imshow("Delta frame", delta_frame)
     cv2.imshow("Threshold frame", threshold_frame)
     cv2.imshow("Color Frame", frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
+
+for i in range(0, len(times), 2):
+    df = df.append({"Start": times[i], "End": times[i + 1]}, ignore_index=True)
+df.to_csv("Times.csv")
 cap.release()
 cv2.destroyAllWindows()
